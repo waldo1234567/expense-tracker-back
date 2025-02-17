ac260185dabc96f7bb6830b6a24d43bafaa2cba3
waldo1234567 <waldowalerian@gmail.com>
2025-02-17 17:35:54 +0800

    setting env

--- src/main/java/com/example/expense/expense_tracker/ExpenseTrackerApplication.java
+++ src/main/java/com/example/expense/expense_tracker/ExpenseTrackerApplication.java
@@ -8,12 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("DB_NAME" , dotenv.get("DB_NAME"));
-		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+
+		System.setProperty("DB_URL", System.getenv("DB_URL"));
+		System.setProperty("DB_USERNAME", System.getenv("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
+		System.setProperty("DB_NAME" , System.getenv("DB_NAME"));
+		System.setProperty("DB_HOST", System.getenv("DB_HOST"));
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
 	}
 
